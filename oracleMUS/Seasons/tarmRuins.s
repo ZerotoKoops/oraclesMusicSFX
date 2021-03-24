621eb4c33eb3e99c85d5a0fc0c4173dd0e6197f1
ZerotoKoops <zerotokoops@gmail.com>
2021-03-23 20:21:55 -0700

    Tarm Ruins

--- oracleMUS/Seasons/tarmRuins.s
+++ oracleMUS/Seasons/tarmRuins.s
@@ -1,7 +1,6 @@
 sound05Start:
-; $ee53d
-; @addr{ee53d}
 sound05Channel1:
+; Measure 1-2
 	vol $0
 	note gs3 $ff
 	vol $0
@@ -10,90 +9,105 @@ sound05Channel1:
 	env $0 $00
 	duty $02
 musicee549:
+; Measure 3
 	vol $6
 	note g5  $30
 	vol $3
 	note g5  $10
+
 	vol $6
 	note fs5 $20
 	note g5  $10
 	note fs5 $10
+; Measure 4
 	note e5  $10
 	wait1 $08
 	vol $3
 	note e5  $08
+
 	vol $6
 	note a4  $10
 	wait1 $08
 	vol $3
 	note a4  $08
+
 	vol $6
 	note a4  $10
 	wait1 $08
 	vol $3
 	note a4  $08
+
 	vol $6
 	note a4  $0c
 	wait1 $04
 	note a4  $08
 	note b4  $08
+; Measure 5
 	note c5  $38
 	vol $3
 	note c5  $08
+
 	vol $6
 	note b4  $20
 	note d5  $20
+; Measure 6
 	note c5  $20
 	note b4  $10
 	note a4  $10
+
 	note b4  $10
 	wait1 $08
 	vol $3
 	note b4  $08
+
 	vol $6
 	note g4  $10
 	wait1 $08
 	vol $3
 	note g4  $08
+; Measure 7
 	vol $6
 	note a4  $30
 	vol $3
 	note a4  $20
 	vol $1
 	note a4  $10
+
 	vol $6
 	note a4  $10
 	note e5  $08
 	wait1 $04
 	vol $3
 	note e5  $04
+; Measure 8
 	vol $6
 	note e5  $30
 	vol $3
-	note e5  $20
+	note e5  $20	
 	vol $1
 	note e5  $10
 	wait1 $10
+
 	vol $6
 	note e5  $08
 	note a5  $08
+; Measure 9
+.rept 2
 	note e5  $20
 	vol $3
 	note e5  $10
+
 	vol $6
 	note e5  $08
 	note a5  $08
-	note e5  $20
-	vol $3
-	note e5  $10
-	vol $6
-	note e5  $08
-	note a5  $08
+.endr
+; Measure 10
 	note e5  $40
 	vol $3
 	note e5  $20
 	vol $1
 	note e5  $20
+; Measure 11
 	vol $6
 	note c6  $08
 	wait1 $04
@@ -102,6 +116,7 @@ musicee549:
 	wait1 $04
 	vol $1
 	note c6  $08
+
 	vol $6
 	note b5  $08
 	wait1 $04
@@ -110,6 +125,7 @@ musicee549:
 	wait1 $04
 	vol $1
 	note b5  $08
+
 	vol $6
 	note a5  $08
 	wait1 $04
@@ -118,6 +134,7 @@ musicee549:
 	wait1 $04
 	vol $1
 	note a5  $08
+
 	vol $6
 	note g5  $08
 	wait1 $04
@@ -126,12 +143,14 @@ musicee549:
 	wait1 $04
 	vol $1
 	note g5  $08
+; Measure 12
 	vol $6
 	note fs5 $48
 	wait1 $08
 	note g5  $10
 	note a5  $10
 	note b5  $10
+; Measure 13
 	note c6  $08
 	wait1 $04
 	vol $3
@@ -139,6 +158,7 @@ musicee549:
 	wait1 $04
 	vol $1
 	note c6  $08
+
 	vol $6
 	note b5  $08
 	wait1 $04
@@ -147,6 +167,7 @@ musicee549:
 	wait1 $04
 	vol $1
 	note b5  $08
+
 	vol $6
 	note a5  $08
 	wait1 $04
@@ -155,6 +176,7 @@ musicee549:
 	wait1 $04
 	vol $1
 	note a5  $08
+
 	vol $6
 	note g5  $08
 	wait1 $04
@@ -163,63 +185,77 @@ musicee549:
 	wait1 $04
 	vol $1
 	note g5  $08
+; Measure 14
 	vol $6
 	note fs5 $40
 	vol $3
 	note fs5 $10
+
 	vol $6
 	note fs5 $10
 	note g5  $10
 	note a5  $10
+; Measure 15
 	note b5  $30
 	vol $3
 	note b5  $10
+
 	vol $6
 	note e6  $30
 	vol $3
 	note e6  $10
+; Measure 16
 	vol $6
 	note d6  $18
 	vol $3
 	note d6  $08
+
 	vol $6
 	note a5  $18
 	vol $3
 	note a5  $08
+
 	vol $6
 	note f5  $18
 	vol $3
 	note f5  $08
+
 	vol $6
 	note d5  $18
 	vol $3
 	note d5  $08
+; Measure 17
 	wait1 $30
+
 	vol $6
 	note b4  $05
 	note e5  $05
 	note a4  $06
 	note b4  $2a
 	wait1 $06
+
 	note b4  $04
 	note e5  $04
 	note fs5 $04
 	note a5  $04
+; Measure 18	
 	note b5  $30
+
 	note a5  $05
 	note b5  $05
 	note a5  $06
+
 	note f5  $20
 	note e5  $10
 	note d5  $10
 	goto musicee549
 	cmdff
-; $ee69d
-; @addr{ee69d}
+
 sound05Channel0:
-	vibrato $00
-	env $0 $00
+	vibrato $00		;pointless?
+	env $0 $00		;pointless?
 	duty $02
+; Measure 1
 	vol $6
 	note a3  $08
 	wait1 $08
@@ -234,6 +270,8 @@ sound05Channel0:
 	note e4  $08
 	vol $3
 	note d4  $08
+; Measure 1c-2
+.rept 3
 	vol $6
 	note a3  $08
 	vol $3
@@ -250,39 +288,10 @@ sound05Channel0:
 	note e4  $08
 	vol $3
 	note d4  $08
-	vol $6
-	note a3  $08
-	vol $3
-	note e4  $08
-	vol $6
-	note b3  $08
-	vol $3
-	note a3  $08
-	vol $6
-	note d4  $08
-	vol $3
-	note b3  $08
-	vol $6
-	note e4  $08
-	vol $3
-	note d4  $08
-	vol $6
-	note a3  $08
-	vol $3
-	note e4  $08
-	vol $6
-	note b3  $08
-	vol $3
-	note a3  $08
-	vol $6
-	note d4  $08
-	vol $3
-	note b3  $08
-	vol $6
-	note e4  $08
-	vol $3
-	note d4  $08
+.endr
 musicee701:
+; Measure 3-6
+.rept 8
 	vol $6
 	note a3  $08
 	vol $3
@@ -299,118 +308,8 @@ musicee701:
 	note e4  $08
 	vol $3
 	note d4  $08
-	vol $6
-	note a3  $08
-	vol $3
-	note e4  $08
-	vol $6
-	note b3  $08
-	vol $3
-	note a3  $08
-	vol $6
-	note d4  $08
-	vol $3
-	note b3  $08
-	vol $6
-	note e4  $08
-	vol $3
-	note d4  $08
-	vol $6
-	note a3  $08
-	vol $3
-	note e4  $08
-	vol $6
-	note b3  $08
-	vol $3
-	note a3  $08
-	vol $6
-	note d4  $08
-	vol $3
-	note b3  $08
-	vol $6
-	note e4  $08
-	vol $3
-	note d4  $08
-	vol $6
-	note a3  $08
-	vol $3
-	note e4  $08
-	vol $6
-	note b3  $08
-	vol $3
-	note a3  $08
-	vol $6
-	note d4  $08
-	vol $3
-	note b3  $08
-	vol $6
-	note e4  $08
-	vol $3
-	note d4  $08
-	vol $6
-	note a3  $08
-	vol $3
-	note e4  $08
-	vol $6
-	note b3  $08
-	vol $3
-	note a3  $08
-	vol $6
-	note d4  $08
-	vol $3
-	note b3  $08
-	vol $6
-	note e4  $08
-	vol $3
-	note d4  $08
-	vol $6
-	note a3  $08
-	vol $3
-	note e4  $08
-	vol $6
-	note b3  $08
-	vol $3
-	note a3  $08
-	vol $6
-	note d4  $08
-	vol $3
-	note b3  $08
-	vol $6
-	note e4  $08
-	vol $3
-	note d4  $08
-	vol $6
-	note a3  $08
-	vol $3
-	note e4  $08
-	vol $6
-	note b3  $08
-	vol $3
-	note a3  $08
-	vol $6
-	note d4  $08
-	vol $3
-	note b3  $08
-	vol $6
-	note e4  $08
-	vol $3
-	note d4  $08
-	vol $6
-	note a3  $08
-	vol $3
-	note e4  $08
-	vol $6
-	note b3  $08
-	vol $3
-	note a3  $08
-	vol $6
-	note d4  $08
-	vol $3
-	note b3  $08
-	vol $6
-	note e4  $08
-	vol $3
-	note d4  $08
+.endr
+; Measure 7
 	vol $6
 	note f3  $08
 	vol $3
@@ -427,6 +326,7 @@ musicee701:
 	note d4  $08
 	vol $3
 	note b3  $08
+
 	vol $6
 	note f4  $08
 	vol $3
@@ -443,6 +343,7 @@ musicee701:
 	note d5  $08
 	vol $3
 	note b4  $08
+; Measure 8	
 	vol $6
 	note f3  $08
 	vol $3
@@ -459,6 +360,7 @@ musicee701:
 	note d4  $08
 	vol $3
 	note b3  $08
+
 	vol $6
 	note f4  $08
 	vol $3
@@ -475,6 +377,7 @@ musicee701:
 	note d5  $08
 	vol $3
 	note b4  $08
+; Measure 9
 	vol $6
 	note e3  $08
 	vol $3
@@ -491,6 +394,7 @@ musicee701:
 	note d4  $08
 	vol $3
 	note b3  $08
+
 	vol $6
 	note e4  $08
 	vol $3
@@ -507,6 +411,7 @@ musicee701:
 	note d5  $08
 	vol $3
 	note b4  $08
+; Measure 10
 	vol $6
 	note e3  $08
 	vol $3
@@ -523,6 +428,7 @@ musicee701:
 	note d4  $08
 	vol $3
 	note b3  $08
+
 	vol $6
 	note e4  $08
 	vol $3
@@ -539,6 +445,7 @@ musicee701:
 	note d5  $08
 	vol $3
 	note b4  $08
+; Measure 11
 	vol $6
 	note ds3 $04
 	wait1 $04
@@ -553,6 +460,7 @@ musicee701:
 	note c4  $04
 	vol $3
 	note a3  $04
+.rept 4
 	vol $6
 	note ds3 $04
 	vol $3
@@ -569,54 +477,8 @@ musicee701:
 	note c4  $04
 	vol $3
 	note a3  $04
-	vol $6
-	note ds3 $04
-	vol $3
-	note c4  $04
-	vol $6
-	note fs3 $04
-	vol $3
-	note ds3 $04
-	vol $6
-	note a3  $04
-	vol $3
-	note fs3 $04
-	vol $6
-	note c4  $04
-	vol $3
-	note a3  $04
-	vol $6
-	note ds3 $04
-	vol $3
-	note c4  $04
-	vol $6
-	note fs3 $04
-	vol $3
-	note ds3 $04
-	vol $6
-	note a3  $04
-	vol $3
-	note fs3 $04
-	vol $6
-	note c4  $04
-	vol $3
-	note a3  $04
-	vol $6
-	note ds3 $04
-	vol $3
-	note c4  $04
-	vol $6
-	note fs3 $04
-	vol $3
-	note ds3 $04
-	vol $6
-	note a3  $04
-	vol $3
-	note fs3 $04
-	vol $6
-	note c4  $04
-	vol $3
-	note a3  $04
+.endr
+; Measure 12b
 	vol $6
 	note fs3 $04
 	vol $3
@@ -633,6 +495,7 @@ musicee701:
 	note ds4 $04
 	vol $3
 	note c4  $04
+
 	vol $6
 	note a3  $04
 	vol $3
@@ -649,6 +512,7 @@ musicee701:
 	note fs4 $04
 	vol $3
 	note ds4 $04
+
 	vol $6
 	note ds4 $04
 	vol $3
@@ -665,10 +529,11 @@ musicee701:
 	note c5  $04
 	vol $3
 	note a4  $04
+; Measure 13
 	vol $6
 	note ds3 $04
 	vol $3
-	note c5  $04
+	note c5  $04		;only difference between M11-12 and M13-14
 	vol $6
 	note fs3 $04
 	vol $3
@@ -681,22 +546,7 @@ musicee701:
 	note c4  $04
 	vol $3
 	note a3  $04
-	vol $6
-	note ds3 $04
-	vol $3
-	note c4  $04
-	vol $6
-	note fs3 $04
-	vol $3
-	note ds3 $04
-	vol $6
-	note a3  $04
-	vol $3
-	note fs3 $04
-	vol $6
-	note c4  $04
-	vol $3
-	note a3  $04
+.rept 4
 	vol $6
 	note ds3 $04
 	vol $3
@@ -713,38 +563,8 @@ musicee701:
 	note c4  $04
 	vol $3
 	note a3  $04
-	vol $6
-	note ds3 $04
-	vol $3
-	note c4  $04
-	vol $6
-	note fs3 $04
-	vol $3
-	note ds3 $04
-	vol $6
-	note a3  $04
-	vol $3
-	note fs3 $04
-	vol $6
-	note c4  $04
-	vol $3
-	note a3  $04
-	vol $6
-	note ds3 $04
-	vol $3
-	note c4  $04
-	vol $6
-	note fs3 $04
-	vol $3
-	note ds3 $04
-	vol $6
-	note a3  $04
-	vol $3
-	note fs3 $04
-	vol $6
-	note c4  $04
-	vol $3
-	note a3  $04
+.endr
+; Measure 14b
 	vol $6
 	note fs3 $04
 	vol $3
@@ -761,6 +581,7 @@ musicee701:
 	note ds4 $04
 	vol $3
 	note c4  $04
+
 	vol $6
 	note a3  $04
 	vol $3
@@ -777,6 +598,7 @@ musicee701:
 	note fs4 $04
 	vol $3
 	note ds4 $04
+
 	vol $6
 	note ds4 $04
 	vol $3
@@ -793,6 +615,7 @@ musicee701:
 	note c5  $04
 	vol $3
 	note a4  $04
+; Measure 15
 	vol $6
 	note g3  $08
 	wait1 $08
@@ -807,6 +630,7 @@ musicee701:
 	note g4  $08
 	vol $3
 	note e4  $08
+
 	vol $6
 	note g3  $08
 	vol $3
@@ -823,6 +647,7 @@ musicee701:
 	note g4  $08
 	vol $3
 	note e4  $08
+; Measure 16
 	vol $6
 	note f3  $08
 	vol $3
@@ -839,6 +664,7 @@ musicee701:
 	note f4  $08
 	vol $3
 	note c4  $08
+
 	vol $6
 	note f3  $08
 	vol $3
@@ -855,6 +681,7 @@ musicee701:
 	note f4  $08
 	vol $3
 	note c4  $08
+; Measure 17
 	vol $6
 	note c3  $08
 	vol $3
@@ -871,6 +698,7 @@ musicee701:
 	note b3  $08
 	vol $3
 	note g3  $08
+
 	vol $6
 	note c3  $08
 	vol $3
@@ -887,6 +715,7 @@ musicee701:
 	note b3  $08
 	vol $3
 	note g3  $08
+; Measure 18
 	vol $6
 	note b2  $08
 	vol $3
@@ -903,6 +732,7 @@ musicee701:
 	note a3  $08
 	vol $3
 	note fs3 $08
+
 	vol $6
 	note as2 $08
 	vol $3
@@ -921,16 +751,19 @@ musicee701:
 	note e3  $08
 	goto musicee701
 	cmdff
-; $eeac1
-; @addr{eeac1}
+	
 sound05Channel4:
 	duty $08
+; Measure 1-3a 		2 measures + 1.125 beats
 	wait1 $ff
 	wait1 $25
 musiceeac7:
+; Measure 3b
 	note g5  $30
 	wait1 $10
+
 	note fs5 $20
+; Measure 4a
 	note g5  $10
 	note fs5 $10
 	note e5  $10
@@ -939,6 +772,7 @@ musiceeac7:
 	wait1 $10
 	note a4  $10
 	wait1 $10
+; Measure 5a
 	note a4  $0c
 	wait1 $04
 	note a4  $08
@@ -946,38 +780,41 @@ musiceeac7:
 	note c5  $38
 	wait1 $08
 	note b4  $20
+; Measure 6a
 	note d5  $20
 	note c5  $20
 	note b4  $10
 	note a4  $10
 	note b4  $10
 	wait1 $10
+; Measure 7a
 	note g4  $10
 	wait1 $10
 	note a4  $60
+; Measure 8a
 	wait1 $10
 	note e5  $08
 	wait1 $08
 	note e5  $60
+; Measure 9a-10a
 	wait1 $10
+.rept 3
 	note e5  $08
 	note a5  $08
 	note e5  $30
-	note e5  $08
-	note a5  $08
-	note e5  $30
-	note e5  $08
-	note a5  $08
-	note e5  $30
+.endr
+; Measure 11a-14a		4 measures + 1.5 beats
 	wait1 $ff
 	wait1 $ff
-	duty $08
+	duty $08		;pointless?
 	wait1 $32
+; Measure 15b
 	note g5  $10
 	note a5  $10
 	note b5  $30
 	wait1 $10
 	note e6  $30
+; Measure 16a
 	note e6  $10
 	note d6  $18
 	wait1 $08
@@ -985,22 +822,26 @@ musiceeac7:
 	wait1 $08
 	note f5  $18
 	wait1 $08
+; Measure 17a
 	note d5  $18
 	wait1 $38
 	note b4  $05
 	note e5  $05
 	note a4  $06
 	note b4  $2a
+; Measure 18a
 	wait1 $06
 	note b4  $04
 	note e5  $04
 	note fs5 $04
 	note a5  $04
+
 	note b5  $30
 	note a5  $05
 	note b5  $05
 	note a5  $06
 	note f5  $20
+; Measure 1a
 	note e5  $10
 	note d5  $10
 	goto musiceeac7
